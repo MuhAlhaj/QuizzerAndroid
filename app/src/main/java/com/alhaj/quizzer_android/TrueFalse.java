15d35f4868991241f4fd3658ef31d8f446ec4020
Muhammad Alhaj <MuhALhaj@gmail.com>
2018-04-19 12:42:08 +0300

    0

--- app/src/main/java/com/alhaj/quizzer_android/TrueFalse.java
+++ app/src/main/java/com/alhaj/quizzer_android/TrueFalse.java
@@ -4,35 +4,21 @@ public class TrueFalse {
     private int mQuestionID;
     private boolean mAnswer;
 
-    /**
-     * @param questionResoueceID
-     * @param trueOrfalse
-     */
     public TrueFalse(int questionResoueceID, boolean trueOrfalse)
     {
         mQuestionID = questionResoueceID;
         mAnswer = trueOrfalse;
     }
 
-    /**
-     * @return
-     */
+
     public int getQuestionID() {
         return mQuestionID;
     }
 
-    /**
-     * @param questionID
-     */
     public void setQuestionID(int questionID) {
         mQuestionID = questionID;
     }
 
-
-
-    /**
-     * @param answer
-     */
     public void setAnswer(boolean answer) {
         mAnswer = answer;
     }
